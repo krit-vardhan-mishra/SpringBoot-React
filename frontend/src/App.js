8e1c59928679aae1384acef5f30fff8fc9a1f487
krit-vardhan-mishra <aboywhoisnerdy@gmail.com>
2025-05-11 17:10:50 +0530

    Trying

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <div className="text-center text-5xl font-bold text-green-700">
+      Hello
+    </div>
+  );
+}
+
+export default App;
